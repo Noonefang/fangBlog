813c4085ca1d1531ad11464912c33d73d93c4aef
Noonefang <839461244@qq.com>
2018-10-20 20:02:39 +0800

    The First Step

---
+++ README.md
@@ -0,0 +1,116 @@
+# 方四方的个人博客计划
+
+```
+material-kit-react
+.
+├── CHANGELOG.md
+├── LICENSE.md
+├── README.md
+├── package.json
+├── Documentation
+│   ├── assets
+│   │   ├── css
+│   │   ├── img
+│   │   │   └── faces
+│   │   └── js
+│   └── tutorial-components.html
+├── public
+│   ├── index.html
+│   └── manifest.json
+└── src
+    ├── index.js
+    ├── logo.svg
+    ├── routes
+    │   └── index.jsx
+    ├── assets
+    │   ├── img
+    │   │   └── kit
+    │   │       ├── faces
+    │   │       └── free
+    │   │           └── examples
+    │   ├── jss
+    │   │   ├── material-kit-react
+    │   │   │   ├── components
+    │   │   │   └── views
+    │   │   │       ├── componentsSections
+    │   │   │       └── landingPageSections
+    │   │   └── material-kit-react.jsx
+    │   └── scss
+    │       ├── core
+    │       │   ├── mixins
+    │       │   └── variables
+    │       ├── plugins
+    │       └── material-kit-react.scss
+    ├── components
+    │   ├── Badge
+    │   │   └── Badge.jsx
+    │   ├── Card
+    │   │   ├── Card.jsx
+    │   │   ├── CardBody.jsx
+    │   │   ├── CardFooter.jsx
+    │   │   └── CardHeader.jsx
+    │   ├── Clearfix
+    │   │   └── Clearfix.jsx
+    │   ├── CustomButtons
+    │   │   ├── Button.jsx
+    │   │   └── IconButton.jsx
+    │   ├── CustomDropdown
+    │   │   └── CustomDropdown.jsx
+    │   ├── CustomInput
+    │   │   └── CustomInput.jsx
+    │   ├── CustomLinearProgress
+    │   │   └── CustomLinearProgress.jsx
+    │   ├── CustomTabs
+    │   │   └── CustomTabs.jsx
+    │   ├── Footer
+    │   │   └── Footer.jsx
+    │   ├── Grid
+    │   │   ├── GridContainer.jsx
+    │   │   └── ItemGrid.jsx
+    │   ├── Header
+    │   │   ├── Header.jsx
+    │   │   └── HeaderLinks.jsx
+    │   ├── InfoArea
+    │   │   └── InfoArea.jsx
+    │   ├── NavPills
+    │   │   └── NavPills.jsx
+    │   ├── Pagination
+    │   │   └── Pagination.jsx
+    │   ├── Snackbar
+    │   │   └── SnackbarContent.jsx
+    │   └── Typography
+    │       ├── Danger.jsx
+    │       ├── Info.jsx
+    │       ├── Muted.jsx
+    │       ├── Primary.jsx
+    │       ├── Quote.jsx
+    │       ├── Small.jsx
+    │       ├── Success.jsx
+    │       └── Warning.jsx
+    └── views
+        ├── Components
+        │   ├── Components.jsx
+        │   └── Sections
+        │       ├── SectionBasics.jsx
+        │       ├── SectionCarousel.jsx
+        │       ├── SectionCompletedExamples.jsx
+        │       ├── SectionDownload.jsx
+        │       ├── SectionExamples.jsx
+        │       ├── SectionJavascript.jsx
+        │       ├── SectionNavbars.jsx
+        │       ├── SectionNotifications.jsx
+        │       ├── SectionPills.jsx
+        │       ├── SectionSignup.jsx
+        │       ├── SectionTabs.jsx
+        │       └── SectionTypography.jsx
+        ├── LandingPage
+        │   ├── LandingPage.jsx
+        │   └── Sections
+        │       ├── ProductSection.jsx
+        │       ├── TeamSection.jsx
+        │       └── WorkSection.jsx
+        ├── ProfilePage
+        │   └── ProfilePage.jsx
+        └── SignupPage
+            └── SignupPage.jsx
+```
---
+++ src/components/Header/HeaderLinks.jsx
@@ -0,0 +1,146 @@
+/*eslint-disable*/
+import React from "react";
+// react components for routing our app without refresh
+import {Link} from "react-router-dom";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import {Photo, Home,OndemandVideo,Book,GroupWork,FlightTakeoff} from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+// todo 作为右边栏
+function HeaderLinks({...props}) {
+    const {classes} = props;
+    return (
+        <List className={classes.list}>
+            <ListItem className={classes.listItem}>
+                <Tooltip
+                    id="instagram-home"
+                    title="主页"
+                    placement={window.innerWidth > 959 ? "top" : "left"}
+                    classes={{tooltip: classes.tooltip}}
+                >
+                    <Button
+                        href="/"
+                        color="transparent"
+                        className={classes.navLink}
+                    >
+                        <Home className={classes.icons}/>
+                    </Button>
+                </Tooltip>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                <Tooltip
+                    id="instagram-photo"
+                    title="相册"
+                    placement={window.innerWidth > 959 ? "top" : "left"}
+                    classes={{tooltip: classes.tooltip}}
+                >
+                    <Button
+                        href="/"
+                        color="transparent"
+                        className={classes.navLink}
+                    >
+                       <Photo className={classes.icons}/>
+                    </Button>
+                </Tooltip>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                <Tooltip
+                    id="instagram-video"
+                    title="视频"
+                    placement={window.innerWidth > 959 ? "top" : "left"}
+                    classes={{tooltip: classes.tooltip}}
+                >
+                    <Button
+                        color="transparent"
+                        href="/"
+                        className={classes.navLink}
+                    >
+                       <OndemandVideo className={classes.icons}/>
+                    </Button>
+                </Tooltip>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                <Tooltip
+                    id="instagram-tooltip"
+                    title="博文"
+                    placement={window.innerWidth > 959 ? "top" : "left"}
+                    classes={{tooltip: classes.tooltip}}
+                >
+                    <Button
+                        color="transparent"
+                        href="/"
+                        target="_blank"
+                        className={classes.navLink}
+                    >
+                        <Book className={classes.icons}/>
+                    </Button>
+                </Tooltip>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                <Tooltip
+                    id="instagram-git"
+                    title="Git"
+                    placement={window.innerWidth > 959 ? "top" : "left"}
+                    classes={{tooltip: classes.tooltip}}
+                >
+                    <Button
+                        href="/"
+                        color="transparent"
+                        target="_blank"
+                        className={classes.navLink}
+                    >
+                        <i className={classes.socialIcons + " fab fa-git"}/>
+                    </Button>
+                </Tooltip>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                <Tooltip
+                    id="instagram-tooltip"
+                    title="应用"
+                    placement={window.innerWidth > 959 ? "top" : "left"}
+                    classes={{tooltip: classes.tooltip}}
+                >
+                    <Button
+                        color="transparent"
+                        href="/"
+                        target="_blank"
+                        className={classes.navLink}
+                    >
+                        <GroupWork className={classes.icons}/>
+                    </Button>
+                </Tooltip>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                <Tooltip
+                    id="instagram-tooltip"
+                    title="计划"
+                    placement={window.innerWidth > 959 ? "top" : "left"}
+                    classes={{tooltip: classes.tooltip}}
+                >
+                    <Button
+                        color="transparent"
+                        href="/"
+                        target="_blank"
+                        className={classes.navLink}
+                    >
+                        <FlightTakeoff className={classes.icons}/>
+                    </Button>
+                </Tooltip>
+            </ListItem>
+
+        </List>
+    );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
